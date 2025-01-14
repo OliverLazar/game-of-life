7ee92b80d50b68443ea7de0c9f2106caf8c9cb26
KingTasaz <kingtasaz123@gmail.com>
2025-01-14 13:28:03 -0500

    cube
    made it centered and fixed lighting

--- Scripts/csg_box_3d.gd
+++ Scripts/csg_box_3d.gd
@@ -1,6 +1,6 @@
 extends CSGBox3D
 
-@export var speed: Vector3 = Vector3(50,50,0)
+@export var speed: Vector3 = Vector3(25,50,40)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
