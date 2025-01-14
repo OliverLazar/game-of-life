209d19f1aa7a5197432a09aa0c6e52d643c12448
KingTasaz <kingtasaz123@gmail.com>
2025-01-14 13:41:08 -0500

    Shakey
    makes the cube shake

--- Scripts/csg_box_3d.gd
+++ Scripts/csg_box_3d.gd
@@ -1,7 +1,10 @@
 extends CSGBox3D
 
 @export var speed: Vector3 = Vector3(25,50,40)
+var time: float = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	rotation_degrees += speed * delta
+	position = Vector3(0, 0.1*sin(time*20), 0)
+	time += delta
