df15bc303786b9428deb1af403a2e39346aa49ee
KingTasaz <kingtasaz123@gmail.com>
2025-02-06 09:48:50 -0500

    FIX
    the whole map got deleted so i wet through commits and fixed it

--- Scripts/Main Menu/playButton.gd
+++ Scripts/Main Menu/playButton.gd
@@ -1,7 +1,8 @@
 extends Button
 
 @onready var sprite = $"../Loading"
-@onready var sound = $"../../AudioStreamPlayer"
+@onready var audio = $"../../AudioStreamPlayer"
+@onready var map = "res://Scenes/Map.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,9 +10,9 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:	
+func _process(delta: float) -> void:
 	if button_pressed:
 		sprite.visible = true
-		sound.play()
+		audio.play()
 		await get_tree().create_timer(0.1).timeout
-		get_tree().change_scene_to_file("res://Scenes/Map.tscn")
+		get_tree().change_scene_to_file(map)
