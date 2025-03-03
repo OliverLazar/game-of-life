f43a012b9e92d82e180ff1b3de04485b5b98abd5
KingTasaz <kingtasaz123@gmail.com>
2025-03-03 14:31:16 -0500

    peepeepoopoo

--- Scripts/Map/GameHandler.gd
+++ Scripts/Map/GameHandler.gd
@@ -32,6 +32,12 @@ var PathChoice = ""  # Stores the path choice for the player (for special square
 var d = Vector3.ZERO  # Target position for the car to move to
 var r = Vector3.ZERO  # Target rotation for the car
 
+var SubEvent  # the blue/red subevent that is currently happening
+var BlueExtA
+var BlueExtB
+
+var rollMaxMaxMax = 10 # spinner debug roll 1-rollMax
+
 # Game state and player tracking variables
 var Players = []  # List of all the player objects
 var Cooldown: int  # General cooldown timer for the game
@@ -63,23 +69,31 @@ func _process(delta: float) -> void:
 			# Tween (smooth transition) the camera to the spinner's position and rotation
 			var cameraTween1 = get_tree().create_tween()
 			var cameraTween2 = get_tree().create_tween()
-			cameraTween1.tween_property(mainCamera, "position", $"SpinnerCameraPOS".position, 3)
-			cameraTween2.tween_property(mainCamera, "rotation", $"SpinnerCameraPOS".rotation, 3)
+			cameraTween1.tween_property(mainCamera, "position", $"SpinnerCameraPOS".position, 1.5)
+			cameraTween2.tween_property(mainCamera, "rotation", $"SpinnerCameraPOS".rotation, 1.5)
 			cameraTween1.play()
 			cameraTween2.play()
 			
 			await get_tree().create_timer(3).timeout  # Wait for the camera transition to finish
 
 		if gameState == 2:  # Move the player's car
+			opA.visible = false
+			opB.visible = false
+			
 			Players[currentPlayerID].car.get_node("carCamera").current = true  # Set the player's car camera active
-			Target = Players[currentPlayerID].spot + 1  # Set the target spot for the player's movement
-			PathChoice = Players[currentPlayerID].spotExt  # Get the path choice from the player
+			
+			var n = Map.get_node(str(Target)+str(PathChoice))
+			if n.has_meta("teleport"):
+				Target = n.get_meta("teleport")
+				PathChoice = n.get_meta("teleportExt")
+			else:
+				Target = Players[currentPlayerID].spot + 1  # Set the target spot for the player's movement
+				PathChoice = Players[currentPlayerID].spotExt  # Get the path choice from the player
 			moveCar(Players[currentPlayerID], Target, PathChoice)  # Move the car to the target
 			Roll -= 1  # Decrease roll after move
 			gameState = 3  # Wait for the car to finish moving
 
 		if gameState == 4:  # Regular event (choose between two options)
-			print("Event")
 			Asel = randi_range(0, len(DebugStandardChoices)-1)  # Randomly select option A
 			Bsel = randi_range(0, len(DebugStandardChoices)-1)  # Randomly select option B
 			while Bsel == Asel:  # Ensure the two options are different
@@ -93,25 +107,66 @@ func _process(delta: float) -> void:
 			
 			opA.visible = true  # Make option A visible
 			opB.visible = true  # Make option B visible
+			
+		if gameState == 6:
+			if SubEvent == "career": # choose career or college
+				# Set the text and value of each option
+				opA.get_node("Text").set_text("Career")
+				opA.get_node("Value").set_text("Free, Quick")
+				opB.get_node("Text").set_text("College")
+				opB.get_node("Value").set_text("$100k, Slow")
+				
+				opA.visible = true  # Make option A visible
+				opB.visible = true  # Make option B visible
+			if SubEvent == "poorJob":
+				BlueExtA = randi_range(0, len(PoorJobs)-1)  # Randomly select option A
+				BlueExtB = randi_range(0, len(PoorJobs)-1)  # Randomly select option B
+				while BlueExtB == BlueExtA:  # Ensure the two options are different
+					BlueExtB = randi_range(0, len(PoorJobs)-1)
+					
+				# Set the text and value of each option
+				opA.get_node("Text").set_text(PoorJobs[BlueExtA]["text"])
+				opA.get_node("Value").set_text(PoorJobs[BlueExtA]["valueText"])
+				opB.get_node("Text").set_text(PoorJobs[BlueExtB]["text"])
+				opB.get_node("Value").set_text(PoorJobs[BlueExtB]["valueText"])
+				
+				opA.visible = true  # Make option A visible
+				opB.visible = true  # Make option B visible
+			if SubEvent == "richJob":
+				BlueExtA = randi_range(0, len(RichJobs)-1)  # Randomly select option A
+				BlueExtB = randi_range(0, len(RichJobs)-1)  # Randomly select option B
+				while BlueExtB == BlueExtA:  # Ensure the two options are different
+					BlueExtB = randi_range(0, len(RichJobs)-1)
+					
+				# Set the text and value of each option
+				opA.get_node("Text").set_text(RichJobs[BlueExtA]["text"])
+				opA.get_node("Value").set_text(RichJobs[BlueExtA]["valueText"])
+				opB.get_node("Text").set_text(RichJobs[BlueExtB]["text"])
+				opB.get_node("Value").set_text(RichJobs[BlueExtB]["valueText"])
+				
+				opA.visible = true  # Make option A visible
+				opB.visible = true  # Make option B visible
+				
 
 	# Handle movement and game state changes during the movement phase
 	if gameState == 3:
 		var p = Players[currentPlayerID]
 		if p.lerpWeight >= 0.2:  # Check if the car has finished moving
-			print("Next Square")
-			var nodeType = Map.get_node(str(p.spot)+str(p.spotExt)).get_meta("Type")
+			var node = Map.get_node(str(p.spot)+str(p.spotExt))
+			var nodeType = node.get_meta("Type")
 			
 			# Different actions based on the type of square the player lands on
 			if nodeType == "payday":
-				print("payday")
 				p.Cash += p.Salary  # Pay the player their salary
 			elif nodeType == "stop":
-				print("stop")
 				gameState = 5  # Transition to stop event
 				gameStateChanged = true
 				Roll = 0
-			elif nodeType == "event":
-				print("fork")
+			elif nodeType == "event":				
+				SubEvent = node.get_meta("Subevent")
+				BlueExtA = node.get_meta("extA")
+				BlueExtB = node.get_meta("extB")
+				
 				gameState = 6  # Branch event
 				gameStateChanged = true
 				RollStoage = Roll  # Store the current roll value for use later
@@ -122,7 +177,6 @@ func _process(delta: float) -> void:
 				gameState = 2
 				gameStateChanged = true
 			elif gameState == 3:
-				print("Done Moving")
 				if nodeType == "default":
 					gameState = 4  # Return to regular event if no special event occurs
 					gameStateChanged = true
@@ -135,13 +189,15 @@ func _process(delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_accept"):
 		if gameState == 1:  # If the game is in the spin state
-			Roll = randi_range(1, 1)  # Generate a random roll
+			Roll = randi_range(1, rollMaxMaxMax)  # Generate a random roll
 			print("Roll: ", Roll)
 			gameState = 2  # Start the movement phase
 			gameStateChanged = true
 
 # Move the car to the target position and rotation
 func moveCar(player, target, ext):
+	target = int(target)
+	
 	var node = Map.get_node(str(target)+str(ext))  # Get the node for the target square
 	
 	# Set the target rotation based on the node's rotation
@@ -169,8 +225,8 @@ func moveCar(player, target, ext):
 
 # Switch to the next player in the game
 func nextPlayer():
-	mainCamera.position = Players[currentPlayerID].car.get_node("carCamera").position
-	mainCamera.rotation = Players[currentPlayerID].car.get_node("carCamera").rotation
+	mainCamera.position = Players[currentPlayerID].car.position + Vector3.UP*4
+	mainCamera.rotation = Players[currentPlayerID].car.rotation
 	
 	mainCamera.current = true  # Set the camera to follow the current player's car
 	opA.visible = false  # Hide options A and B
@@ -203,12 +259,12 @@ func _start_button():
 	# Animate the camera to the start position
 	var cameraTween1 = get_tree().create_tween()
 	var cameraTween2 = get_tree().create_tween()
-	cameraTween1.tween_property(mainCamera, "position", $"StartCameraPOS".position, 3)
-	cameraTween2.tween_property(mainCamera, "rotation", $"StartCameraPOS".rotation, 3)
+	cameraTween1.tween_property(mainCamera, "position", $"StartCameraPOS".position, 1)
+	cameraTween2.tween_property(mainCamera, "rotation", $"StartCameraPOS".rotation, 1)
 	cameraTween1.play()
 	cameraTween2.play()
 	
-	await get_tree().create_timer(4).timeout  # Wait for the animation to complete
+	await get_tree().create_timer(1.4).timeout  # Wait for the animation to complete
 
 	# Set up each player's camera
 	for p in range(PlayerCount):
@@ -227,18 +283,52 @@ func _start_button():
 # Option A button handler
 func _optionA():
 	if gameState == 4:  # If the game is in event state
-		print(DebugStandardChoices[Asel]["text"])  # Display option A's text
 		gameState = 1  # Move to spinner state
 		gameStateChanged = true
 		nextPlayer()  # Move to the next player
+	if gameState == 6:
+		if SubEvent == "career":
+			# career
+			pass
+		if SubEvent == "poorJob":
+			Players[currentPlayerID].Salary = PoorJobs[BlueExtA]["value"]
+		if SubEvent == "richJob":
+			Players[currentPlayerID].Salary = RichJobs[BlueExtA]["value"]
+			
+		if RollStoage > 0:
+			Roll = RollStoage
+			gameState = 2
+			gameStateChanged = true
+		else:
+			gameState = 1  # Move to spinner state
+			gameStateChanged = true
+			nextPlayer()  # Move to the next player
+			
 
 # Option B button handler
 func _optionB():
 	if gameState == 4:  # If the game is in event state
-		print(DebugStandardChoices[Bsel]["text"])  # Display option B's text
 		gameState = 1  # Move to spinner state
 		gameStateChanged = true
 		nextPlayer()  # Move to the next player
+	if gameState == 6:
+		if SubEvent == "career":
+			# college
+			Players[currentPlayerID].spotExt = BlueExtB
+		if SubEvent == "poorJob":
+			Players[currentPlayerID].Salary = PoorJobs[BlueExtB]["value"]
+		if SubEvent == "richJob":
+			Players[currentPlayerID].Salary = RichJobs[BlueExtB]["value"]
+			
+		if RollStoage > 0:
+			Roll = RollStoage
+			gameState = 2
+			gameStateChanged = true
+		else:
+			gameState = 1  # Move to spinner state
+			gameStateChanged = true
+			nextPlayer()  # Move to the next player
+			
 
 # Example of predefined standard choices for event options
 var DebugStandardChoices = [
@@ -246,4 +336,16 @@ var DebugStandardChoices = [
 	{"text":"Roderick", "value":26, "valueText": "$26"},
 	{"text":"Ayoub", "value":-48, "valueText": "-$48"},
 	{"text":"Oliver", "value":696969, "valueText": "$696969"}
+]
+
+# Jobs that dont pay well
+var PoorJobs = [
+	{"text":"Garbage Man", "value":1000, "valueText": "$1000"},
+	{"text":"Barista", "value":2000, "valueText": "$2000"}
+]
+
+# Jobs that do pay well
+var RichJobs = [
+	{"text":"Doctor", "value":125000, "valueText": "$125k"},
+	{"text":"Lawyer", "value":110000, "valueText": "$110k"}
 ]
